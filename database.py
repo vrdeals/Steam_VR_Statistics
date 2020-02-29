93c906969cb627796bacd3fec21f5509d1b0d1dd
Bamu <bamueq@googlemail.com>
2020-02-29 16:25:12 +0100

    update

--- database.py
+++ database.py
@@ -3,22 +3,24 @@ def create_database(c):
     c.execute('''CREATE TABLE IF NOT EXISTS vr_players (appid integer NOT NULL, date text NOT NULL, players real)''')
 
 
-def check_game(c, val):
-    c.execute('SELECT * FROM vr_games WHERE appid=?', val)
+# def check_game(c, val):
+#     c.execute('SELECT * FROM vr_games WHERE appid=?', val)
+#     return c.fetchone()
+
+
+def last_update(c):
+    c.execute('SELECT max(date) FROM vr_players')
     return c.fetchone()
 
 
 def add_game(c, val):
     c.execute('''INSERT INTO vr_games(appid,title) VALUES(?,?)''', val)
-    return c.lastrowid
 
 
 def add_players(c, val):
     c.execute('''INSERT INTO vr_players(appid,date,players) VALUES(?,?,?)''', val)
-    return c.lastrowid
 
 
-def last_update(c):
-    c.execute("SELECT max(date) FROM vr_players Limit 1")
-    print(c.fetchone()[0])
-    return c.fetchone()
+def reset(c):
+    c.execute('DELETE FROM vr_games;')
+    c.execute('DELETE FROM vr_players;')
--- main.py
+++ main.py
@@ -27,14 +27,14 @@ class Game:
             database.add_players(c, (self.appid, date, players))
 
 
-def get_vrgames_steam(c):
+def get_vrgames_steam():
     """Get the appid and name of all steam VR games with a vr only tag, sorted by release date(desc)"""
     infinite_scrolling = 0
     counter = 0
     games = []
     while True:
         url = 'https://store.steampowered.com/search/results/?query&start=' + str(infinite_scrolling) + \
-                '&count=100&dynamic_data=&sort_by=Released_DESC&force_infinite=1&vrsupport=401&snr=1_7_7_230_7&' \
+                '&count=50&dynamic_data=&sort_by=Released_DESC&force_infinite=1&vrsupport=401&snr=1_7_7_230_7&' \
                 'infinite=1'
         json_data = json.loads(requests.get(url).text)
         if json_data["results_html"] != "\r\n<!-- List Items -->\r\n<!-- End List Items -->\r\n":
@@ -43,12 +43,9 @@ def get_vrgames_steam(c):
             game_list = tree.xpath('//span[@class="title"]/text()')
             for appid, game in zip(appid_list, game_list):
                 counter += 1
-                if database.check_game(c, (appid,)) is None:
-                    print(counter, appid, game)
-                    games.append(Game(appid, game))
-                else:
-                    print(counter, appid, game, "already in the database")
-            infinite_scrolling += 100
+                print(counter, appid, game)
+                games.append(Game(appid, game))
+            infinite_scrolling += 50
         else:
             return games
 
@@ -61,10 +58,6 @@ def get_vrgames_players(appid):
     tree = html.fromstring(page.content)
     date_list = tree.xpath('//td[@class="month-cell left"]/text()')
     players_list = tree.xpath('//td[@class="right num-f"]/text()')
-    last_30_days = tree.xpath('//td[@class="right num-f italic"]/text()')
-    now = datetime.date.today()
-    if last_30_days:
-        players.append([now.strftime('%Y-%m'), last_30_days[0]])
     if players_list:
         for date, avg_players in zip(date_list, players_list):
             date = date.strip().split(" ")
@@ -77,19 +70,40 @@ def get_vrgames_players(appid):
     return players
 
 
+def update_required(c):
+    """checks if more than 1 month has passed since the last update"""
+    update = False
+    today = datetime.date.today()
+    today = int(today.strftime('%m'))
+    last_update = database.last_update(c)[0]
+    if last_update is None:
+        update = True
+    else:
+        last = int(last_update.split("-")[1])
+        delta = today - last
+        if delta > 1:
+            update = True
+    return update
+
+
 def main():
     database_location = 'vr_games_database.db'
     conn = sqlite3.connect(database_location)
     c = conn.cursor()
     database.create_database(c)
-    database.last_update(c)
-    games = get_vrgames_steam(c)
-    for game in games:
-        game.number_of_players()
-        game.print_game()
-        game.add_game_to_database(c)
+    if update_required(c):
+        print("The database will be updated.")
+        games = get_vrgames_steam()
+        for game in games:
+            game.number_of_players()
+            game.print_game()
+        database.reset(c)
+        for game in games:
+            game.add_game_to_database(c)
     conn.commit()
     conn.close()
+    print("The database is up to date. "
+          "All VR only games and the number of players until the end of last month are included in the statistics.")
 
 
 if __name__ == "__main__":
