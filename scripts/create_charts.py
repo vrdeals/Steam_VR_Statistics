2127b8401a0b34d5d3d0dc36f9bcf255564e5c4a
Bamu <bamueq@googlemail.com>
2020-03-06 16:03:42 +0100

    update

--- scripts/create_charts.py
+++ scripts/create_charts.py
@@ -28,7 +28,7 @@ def peak_players_online_sql(cursor):
     WHERE date != '2019-07-24' and date > '2016-03'
     Group by appid, new_date
     Order by new_date)
-    where average > 2
+    where average > 3
     Group by new_date
     ''')
     return cursor.fetchall()
