bd51d33e4b9de961664fd6a1d9c20011690d7d42
Bamu <bamueq@googlemail.com>
2020-04-04 23:04:49 +0200

    update

--- src/update_database.py
+++ src/update_database.py
@@ -35,7 +35,7 @@ def get_new_vrgames_steam():
         tree = html.fromstring(json_data["results_html"])
         appid_list = tree.xpath('//a/@data-ds-appid')
         game_list = tree.xpath('//span[@class="title"]/text()')
-        if games := check_appids_existing(appid_list, game_list):
+        if games := check_appids_exist(appid_list, game_list):
             new_games.extend(games)
         else:
             break
